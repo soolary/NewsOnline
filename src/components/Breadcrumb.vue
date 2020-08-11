d22fa6c1a0eadcad1ebd1ee879ef3caad4320e95
wangjie <770499571@qq.com>
2020-08-11 19:30:04 +0800

    葫芦娃头条1.0

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,9 +1,7 @@
 <template>
   <el-breadcrumb separator-class="el-icon-arrow-right">
     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item v-if="$route.path != '/'">{{
-      $route.meta.title
-    }}</el-breadcrumb-item>
+    <el-breadcrumb-item>{{ $route.meta.title }}</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
